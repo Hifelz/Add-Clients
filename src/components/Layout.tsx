dbf60dd15f0d371fd3d0e2cde1718676f6aff625
Hifelz <100286956+Hifelz@users.noreply.github.com>
2023-05-11 10:06:15 -0300

    style: added css on title and layout

--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -10,9 +10,9 @@ export default function Layout (props: LayoutProps) {
    
     return (
         <div className={'flex flex-col w-2/3 bg white text gray-800'}>
-            <Title></Title>
-            <div>
-
+            <Title>{props.title}</Title>
+            <div className="p-6">
+                {props.childre}
             </div>
         </div>
     )
